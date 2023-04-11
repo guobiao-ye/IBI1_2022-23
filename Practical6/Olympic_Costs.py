c1f53b5e4e928b863890742be971efc73a2dc149
Ye Guobiao <guobiao.22@intl.zju.edu.cn>
2023-04-12 00:23:41 +0800

    Practical 6

--- Practical6/Olympic_Costs.py
+++ Practical6/Olympic_Costs.py
@@ -2,21 +2,35 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-# Define the number of data points, and the data itself of corresponding cost
+# Define the number of data points
 N = 8
+
+# Olympic Games and corresponding cost correspond one to one by creating a list of (game, cost) tuples
+olympic_games = ['Los Angeles 1984', 'Seoul 1988', 'Barcelona 1992', 'Atlanta 1996', 'Sydney 2000', 'Athens 2003', 'Beijing 2008', 'London 2012']
 costs = [1, 8, 15, 7, 5, 14, 43, 40]
+data = list(zip(olympic_games, costs))
+
+# Sort the data based on cost
+sorted_data = sorted(data, key=lambda x: x[1])
+
+# Extract sorted games and costs from sorted data
+sorted_olympic_games = [game for game, cost in sorted_data]
+sorted_costs = [cost for game, cost in sorted_data]
+
 # Set up the plot
 ind = np.arange(N)
 width = 0.35
-pl = plt.bar(ind, costs, width)
+pl = plt.bar(ind, sorted_costs, width)
+
 # Set the title and axis labels for the plot
-plt.ylabel('costs')
+plt.ylabel('Costs (in $ billions)')
 plt.title('Olympic Costs')
-plt.xticks(ind, ('Los Angeles', 'Seoul', 'Barcelona', 'Atlanta', 'Sydney', 'Athens', 'Beijing', 'London'), rotation=45)
+plt.xticks(ind, sorted_olympic_games, rotation=45)
+
 # Set the length and scale of the vertical axis
 plt.yticks(np.arange(0, 51, 10))
 
 # Display the plot
 plt.show()
 # Print the costs
-print(costs)
+print(sorted_costs)
