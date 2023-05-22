d2d28cd31c381dfaf6fc55d1fcd03b6f0b0bbf9c
guobiao-ye <127282285+guobiao-ye@users.noreply.github.com>
2023-05-22 23:58:03 +0800

    Update count_cds_genes.py

--- Practical9/count_cds_genes.py
+++ Practical9/count_cds_genes.py
@@ -1,44 +1,51 @@
-# Ask user to input stop codon
-stop_codon = input("Please enter one of the three stop codons (TAA, TAG, TGA): ")
+# Ask the user to input the stop codon
+stop_codon = input("Enter one of the three stop codons (TAA, TAG, TGA): ")
 
-# Check if the input stop codon is valid
-if stop_codon not in ['TAA', 'TAG', 'TGA']:
-    print("Invalid stop codon. Please enter either TAA, TAG, or TGA.")
-else:
-    # Create output filename based on stop codon
-    output_file = f"{stop_codon}_stop_genes.fa"
+# Create the output file name
+output_file = f"{stop_codon}_stop_genes.fa"
 
-    # Input file path and name
-    input_file = r'Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa'
+# Open the input file for reading
+with open("Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa", "r") as input_file:
+    # Open the output file for writing
+    with open(output_file, "w") as output_file:
+        # Read the input file line by line
+        lines = input_file.readlines()
+        
+        # Initialize variables
+        gene_name = ""
+        coding_sequence_count = 0
+        sequence = ""
+        
+        # Process each line in the input file
+        for line in lines:
+            line = line.strip()
+            
+            # Check if the line contains the gene information
+            if line.startswith(">"):
+                # Check if we have a previous gene sequence to write
+                if gene_name != "":
+                    # Write the gene sequence in FASTA format to the output file
+                    output_file.write(f">{gene_name} ({coding_sequence_count} coding sequences)\n")
+                    output_file.write(f"{sequence}\n")
+                
+                # Extract the gene name from the line
+                gene_name = line[1:].split(" ")[0]
+                
+                # Reset the coding sequence count and sequence
+                coding_sequence_count = 0
+                sequence = ""
+            
+            # Check if the line contains the sequence
+            else:
+                # Append the sequence to the existing sequence
+                sequence += line
+            
+                # Count the number of coding sequences
+                coding_sequence_count += line.count(stop_codon)
+        
+        # Write the last gene sequence to the output file
+        output_file.write(f">{gene_name} ({coding_sequence_count} coding sequences)\n")
+        output_file.write(f"{sequence}\n")
 
-    # Open input file for reading
-    with open(input_file, 'r') as f_in:
-        # Open output file for writing
-        with open(output_file, 'w') as f_out:
-            # Initialize variables to store gene name and sequence count
-            gene_name = None
-            sequence_count = 0
-            # Loop through each line in the input file
-            for line in f_in:
-                # Check if the line starts with '>' indicating a new gene entry
-                if line.startswith('>'):
-                    # Check if there is a previous gene entry
-                    if gene_name is not None and gene_name.endswith(stop_codon):
-                        # Write the gene name and sequence count to the output file in FASTA format
-                        f_out.write(f'>{gene_name} ({sequence_count})\n')
-                    # Extract the gene name from the line and remove other information
-                    gene_name = line[1:].strip().split(' ')[0]
-                    # Reset the sequence count variable
-                    sequence_count = 0
-                else:
-                    # Concatenate the line to the sequence
-                    f_out.write(line.strip())
-                    sequence_count += 1
-
-            # Check if there is a last gene entry at the end of the file
-            if gene_name is not None and gene_name.endswith(stop_codon):
-                # Write the last gene name and sequence count to the output file in FASTA format
-                f_out.write(f'>{gene_name} ({sequence_count})\n')
-
-    print(f"Extraction of genes ending with stop codon '{stop_codon}' completed.")
-    print(f"Results saved in '{output_file}'")
+print(f"Extraction of genes ending with stop codon '{stop_codon}' completed.")
+print(f"Results saved in '{output_file.name}'")
